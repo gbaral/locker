c1a686ebd1a7f7c81239fdc92540d4173c9d7d80
sbaral <sbaral@BAGMATI>
2014-02-13 01:31:10 -0600

    firstchange

---
+++ nepal/src/bagmati/bhaktapur.java
@@ -0,0 +1,11 @@
+ package bagmati;
+
+public class bhaktapur {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("Hello World");
+		 System.out.println("Nepal"); 
+	}
+
+}
